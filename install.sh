3024ad20a27ba356532ff2ee2e4d14474b70d376
thxrhmn <rahmanmuhaemin@gmail.com>
2023-07-22 01:50:20 +0000

    change

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 echo -e "${GREEN}::::::::::INSTALL RCLONE:::::::::::::${ENDCOLOR}"
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
+#install rclone
+if [ $(dpkg-query -W -f='${Status}' rclone 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+  sudo apt install ./rclone.deb;
+fi
+
+rclone config file
+#download rclone.conf from gitpod .env
+FILE=/home/gitpod/.config/rclone/rclone.conf
+if [ -f "$FILE" ]; then  
+    echo "$FILE exist"  
+else  
+    curl $RCLONE_CONFIG_URL -o /home/gitpod/.config/rclone/rclone.conf 
+fi  
+
+rclone config
